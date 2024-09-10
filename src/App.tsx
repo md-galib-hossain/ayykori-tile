0b017ae8116603fe1f35cd3def3be1b0c243785c
Md Galib Hossain <mdgalib23@gmail.com>
2024-09-10 20:43:54 +0600

    Feat: Folder structure initiate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+
+function App() {
+
+  return (
+    <div className='container mx-auto'>
+    <h2 className=' text-2xl text-red-500'>Check</h2>
+    </div>
+  )
+}
+
+export default App
